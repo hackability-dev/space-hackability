8eaa0ff5071c7c849f07ebce119cc56cf8a44504
Ludovico Russo <ludovico@ludusrusso.space>
2021-02-27 16:52:09 +0100

    add graphql code gen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
-import { GetServerSideProps, GetStaticProps } from 'next';
+import { GetServerSideProps, GetStaticProps, InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { GetGroupsDocument } from '../src/generated/graphql';
+import { FC } from 'react';
 
 const client = new ApolloClient({
   uri: 'https://space.k8s.hackability.dev/api/graphql',
   cache: new InMemoryCache(),
 });
 
-const Home = ({ groups }) => {
+const Home: FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ groups }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,24 +19,21 @@ const Home = ({ groups }) => {
       </Head>
       <h1>hackability</h1>
       <p> Welcome CI/CD</p>
-      <pre>{JSON.stringify(groups, null, 2)}</pre>
+      {groups.map((group) => (
+        <div key={group.id}>
+          <h3>{group.name}</h3>
+          <p>{group.description}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default Home;
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getStaticProps = async () => {
   const res = await client.query({
-    query: gql`
-      query GetGroups {
-        groups {
-          id
-          description
-          name
-        }
-      }
-    `,
+    query: GetGroupsDocument,
   });
   return {
     props: {
---
+++ src/generated/graphql.ts
@@ -0,0 +1,60 @@
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Group = {
+  __typename?: 'Group';
+  id: Scalars['String'];
+  name: Scalars['String'];
+  description: Scalars['String'];
+  image: Scalars['String'];
+  users: Array<UserGroup>;
+};
+
+export type UserGroup = {
+  __typename?: 'UserGroup';
+  role: Scalars['String'];
+  user: User;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['String'];
+  name: Scalars['String'];
+  email: Scalars['String'];
+  bio: Scalars['String'];
+  description: Scalars['String'];
+  linkedin?: Maybe<Scalars['String']>;
+  twitter?: Maybe<Scalars['String']>;
+  github?: Maybe<Scalars['String']>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  groups: Array<Group>;
+  hello: Scalars['String'];
+};
+
+export type GetGroupsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetGroupsQuery = (
+  { __typename?: 'Query' }
+  & { groups: Array<(
+    { __typename?: 'Group' }
+    & Pick<Group, 'id' | 'description' | 'name'>
+  )> }
+);
+
+
+export const GetGroupsDocument: DocumentNode<GetGroupsQuery, GetGroupsQueryVariables> = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetGroups"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"groups"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"description"}},{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]};
