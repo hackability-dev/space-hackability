c6f40000e4e5d4d77496be227435550cd9042c51
Ludovico Russo <ludovico@ludusrusso.space>
2021-03-20 15:31:20 +0100

    rm prisma

--- prisma/migrations/20210213143416_add_group/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Group" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL DEFAULT E'',
-    "image" TEXT NOT NULL,
-
-    PRIMARY KEY ("id")
-);
